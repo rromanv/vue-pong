598115cdab711e96beff6a3057b7ed327848c3ec
Rodolfo Roman <rromanv@gmail.com>
2020-04-09 11:04:20 -0500

    Main Layout

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,11 @@
 <template>
-  <div id="app">
-    <router-view />
-  </div>
+  <v-app>
+    <v-app-bar app clipped-left dark color="primary">
+      <v-toolbar-title>VuePong</v-toolbar-title>
+    </v-app-bar>
+    <v-navigation-drawer app clipped class="yellow ligthten-1"> </v-navigation-drawer>
+    <v-content class="error noOverflow">
+      <router-view />
+    </v-content>
+  </v-app>
 </template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,5 @@
 <template>
-  <div class="home">
-    <h1>Hello There</h1>
-  </div>
+  <h1>Hello There</h1>
 </template>
 
 <script>
